bf470aca6a2b6216172a2d97e899f28cf67e1a79
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-28 10:39:40 -0300

    creo models

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Voluntario(models.Model):
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+    voluntarios = models.ManyToManyField(Voluntario, related_name='eventos')
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ['-fecha']

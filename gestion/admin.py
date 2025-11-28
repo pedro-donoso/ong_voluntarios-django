16814f7e90199cc083f4f355fcbe4a451e9a96d9
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-28 11:18:42 -0300

    creo admin

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Voluntario, Evento
+
+@admin.register(Voluntario)
+
+
+class VoluntarioAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'telefono', 'fecha_registro']
+    search_fields = ['nombre', 'email']
+    list_filter = ['fecha_registro']
+
+@admin.register(Evento)
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'fecha', 'get_voluntarios_count']
+    filter_horizontal = ['voluntarios']
+    list_filter = ['fecha']
+
+    def get_voluntarios_count(self, obj):
+        return obj.voluntarios.count()
+    get_voluntarios_count.short_description = 'Voluntarios'
